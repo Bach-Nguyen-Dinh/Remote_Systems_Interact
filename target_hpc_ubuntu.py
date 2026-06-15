e270a29ec48f6356b2b30f0a612bcf0b7a724f6d
Bach-Nguyen-Dinh <bachdinhnguyen.work@gmail.com>
2026-06-15 14:07:21 +0930

    fix network interface id

--- target_hpc_ubuntu.py
+++ target_hpc_ubuntu.py
@@ -458,7 +458,7 @@ def listen_for_messages():
                         interface_id = LW_ETH_OB_INTERFACE_ID
                     elif target == "UpEthOnb":
                         interface_id = UP_ETH_OB_INTERFACE_ID
-                    elif target == "LwEthAdt" or FM_INTERFACE_ID:
+                    elif target == "LwEthAdt" or target == FM_INTERFACE_ID:
                         interface_id = LW_ETH_ADT_INTERFACE_ID
                     elif target == "UpEthAdt":
                         interface_id = UP_ETH_ADT_INTERFACE_ID
